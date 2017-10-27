debe22bf67f27ef7c7d5dccbff72853963613bba
Michael Chen <chenpion@umich.edu>
2017-10-27 12:28:42 -0400

    Add subtrack implementation

--- rpn.py
+++ rpn.py
@@ -2,15 +2,20 @@
 
 def calculate(myarg):
 	stack = list()
-	for token in myarg.split();
+	for token in myarg.split():
 		if token == '+':
 			arg1 = stack.pop()
 			arg2 = stack.pop()
 			result = arg1 + arg2
 			stack.append(result)
+		elif token == '-':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 - arg2
+			stack.append(result)
 		else:
 			stack.append(int(token))
-	print[stack]
+	#print(stack)
 	return stack.pop()
 
 def main():
