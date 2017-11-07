1f79be8db45a3889a8b03391f2161dc140c9614a
Michael Chen <chenpion@umich.edu>
2017-11-07 00:06:49 -0500

    yay

--- test_rpn.py
+++ test_rpn.py
@@ -9,3 +9,6 @@ class TestBasics(unittest.TestCase):
 	def test_subtrack(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+	def test_carrot(self):
+		result = rpn.calculate('1 2 ^')
+		self.assertEqual(1, result)
